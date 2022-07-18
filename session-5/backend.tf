599fdab88da8efb5e91a9d48074e1c2a4b745e81
Turdukan <turdukan98@gmail.com>
2022-07-18 23:33:26 +0000

    Added session-5

---
+++ session-5/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-session-remote-backend-bucket-turdukan"
+    key            = "session-5/terraform.tfstate" # Prefix (folder)
+    region         = "us-west-2"
+    dynamodb_table = "terraform-june-2022-state-lock-table"
+  }
+}
+
+
+
+# Terraform state lock prevents from the accidental apply or destroy commands or state changes
+# It will make sure one apply or destroy at the times. 
+# Terraform state lock uses DynomaDB table to lock every single executions
