5ee89cc953f235f4a9adf2fb72588867530e768b
Turdukan <turdukan98@gmail.com>
2022-07-26 02:04:08 +0000

    Added session-10

--- session-10/main.tf
+++ session-10/main.tf
@@ -4,8 +4,9 @@ module "s3" {
   my_name = "turdukan"
 }
 module "ec2" {
-  source = "github.com/Turdukan/terraform-june-2022/modules/ec2"
-  ami           = "ami-098e42ae54c764c35"
-  instance_type = "t2.micro"
-  env           = "dev"
+  source         = "github.com/Turdukan/terraform-june-2022/modules/ec2"
+  ami            = "ami-098e42ae54c764c35"
+  instance_type  = "t2.micro"
+  env            = "dev"
+  s3_bucket_name = module.s3.bucket_name
 }
